d184c062d1e842bcc0c581aa450968654a621172
NepyAnna <nepyann@gmail.com>
2024-10-29 11:13:24 +0100

    add constructor and get functions

--- src/main/java/com/sheoanna/HandlerOfWeekDays.java
+++ src/main/java/com/sheoanna/HandlerOfWeekDays.java
@@ -1,7 +1,20 @@
 package com.sheoanna;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HandlerOfWeekDays {
     public List<String> weekDays;
+
+    public HandlerOfWeekDays() {
+        createListOfWeekDays();
+    }
+
+    public List<String> getWeekDays() {
+        return weekDays;
+    }
+
+    public void createListOfWeekDays() {
+
+    }
 }
